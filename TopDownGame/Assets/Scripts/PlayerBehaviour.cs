6c54d4f5ad066beb797a5a88e5e832ff5b9bf74f
Szilard Ficsor <fszili0514@gmail.com>
2023-11-09 15:24:39 +0100

    Simple Animations

--- TopDownGame/Assets/Scripts/PlayerBehaviour.cs
+++ TopDownGame/Assets/Scripts/PlayerBehaviour.cs
@@ -11,16 +11,20 @@ public class PlayerBehaviour : MonoBehaviour
     private Vector3 _mousePosition;
     private Vector2 _lookDirection;
     private Camera _camera;
+    private bool _hasGun = false;
+
+    private Animator _animator;
     void Start()
     {
-        _rb= GetComponent<Rigidbody2D>();
-        _camera= Camera.main;
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponentInChildren<Animator>();
+        _camera = Camera.main;
     }
 
     void Update()
     {
         ProcessInputs();
-       
+        AnimationHandeler();
     }
     private void FixedUpdate()
     {
@@ -32,19 +36,42 @@ public class PlayerBehaviour : MonoBehaviour
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
 
-        _moveDirection= new Vector2(inputX, inputY).normalized;
+        _moveDirection = new Vector2(inputX, inputY).normalized;
 
         _mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
         _lookDirection = _mousePosition - transform.position;
     }
     private void Move()
     {
-        _rb.velocity = new Vector2(_moveDirection.x,_moveDirection.y) * _movementSpeed;
+        _rb.velocity = new Vector2(_moveDirection.x, _moveDirection.y) * _movementSpeed;
     }
     private void Rotate()
     {
         float angle = Mathf.Atan2(_lookDirection.y, _lookDirection.x) * Mathf.Rad2Deg;
         angle -= 90; // Correction degrees
         transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+    private void AnimationHandeler()
+    {
+        if (_rb.velocity != Vector2.zero)
+        {
+            _animator.SetBool("Walk", true);
+        }
+        else
+        {
+            _animator.SetBool("Walk", false);
+        }
+
+
+        if (Input.GetMouseButtonDown(1) && !_hasGun)
+        {
+            _animator.SetBool("IsHoldingGun", true);
+            _hasGun = true;
+        }
+        else if (Input.GetMouseButtonDown(1) && _hasGun)
+        {
+            _animator.SetBool("IsHoldingGun", false);
+            _hasGun = false;
+        }
     }
 }
